43e7f38e283f3752a8208d3a680edf876b3f5ac5
junghokim <royalstyles@naver.com>
2022-07-01 17:45:00 +0900

    카카오 맵 MapView 1차 업데이트

--- app/src/main/java/com/example/todo_app/ui/map/MapFragment.java
+++ app/src/main/java/com/example/todo_app/ui/map/MapFragment.java
@@ -1,29 +1,50 @@
 package com.example.todo_app.ui.map;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
-
 import com.example.todo_app.PermissionSupport;
 import com.example.todo_app.R;
 import com.example.todo_app.databinding.FragmentMapBinding;
 
 import net.daum.mf.map.api.MapPoint;
+import net.daum.mf.map.api.MapReverseGeoCoder;
 import net.daum.mf.map.api.MapView;
 
-public class MapFragment extends Fragment {
+public class MapFragment extends Fragment implements MapView.CurrentLocationEventListener, MapReverseGeoCoder.ReverseGeoCodingResultListener {
 
     private FragmentMapBinding binding;
     private View root;
 
     // 권한 클래스를 선언
     private PermissionSupport permissionSupport;
+
+    private Activity activity;
+    private Context context;
+    private MapView mapView;
+
+    private static final int GPS_ENABLE_REQUEST_CODE = 1000;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        activity = getActivity();
+        context = getContext();
+    }
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -37,7 +58,7 @@ public class MapFragment extends Fragment {
 //        mapViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
 
         // 권한 객체 생성
-        permissionSupport = new PermissionSupport(getActivity(), getContext());
+        permissionSupport = new PermissionSupport(activity, context);
 
         // 권한 체크
         permissionSupport.checkAll();
@@ -58,22 +79,103 @@ public class MapFragment extends Fragment {
     //맵 뷰를 띄워준다.
     private void gotoMap(){
         Log.d(getClass().getName(), "KJH : " + Thread.currentThread().getStackTrace()[2].getMethodName());
-        MapView mapView = new MapView(getActivity());
+
+        mapView = new MapView(activity);
 
         // 중심점 변경
 //        mapView.setMapCenterPoint(MapPoint.mapPointWithGeoCoord(37.53737528, 127.00557633), true);
-        mapView.setMapCenterPoint(MapPoint.mapPointWithGeoCoord(37.53737528, 127.00557633), true);
+//        mapView.setMapCenterPoint(MapPoint.mapPointWithGeoCoord(37.53737528, 127.00557633), true);
 
         ViewGroup mapViewContainer = root.findViewById(R.id.map_view);
         mapViewContainer.addView(mapView);
 
+        mapView.setCurrentLocationEventListener(this);
 
-        // 줌 버튼
         // 줌 레벨 변경
         mapView.setZoomLevel(7, true);
+
+        // 현재 위치
+//        mapView.setCurrentLocationTrackingMode(MapView.CurrentLocationTrackingMode.TrackingModeOnWithoutHeading);
+
+        // GPS 활성화 요청
+        if (!checkLocationServiceStatus()) {
+            showDialogForLocationServiceSetting();
+        }
+
         // 줌 인
         mapView.zoomIn(true);
         // 줌 아웃
         mapView.zoomOut(true);
     }
+
+    @Override
+    public void onReverseGeoCoderFoundAddress(MapReverseGeoCoder mapReverseGeoCoder, String s) {
+        Log.d(getClass().getName(), "KJH : " + Thread.currentThread().getStackTrace()[2].getMethodName());
+        mapReverseGeoCoder.toString();
+        Toast.makeText(activity, "Reverse Geo-coding : " + s, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onReverseGeoCoderFailedToFindAddress(MapReverseGeoCoder mapReverseGeoCoder) {
+
+    }
+
+    @Override
+    public void onCurrentLocationUpdate(MapView mapView, MapPoint mapPoint, float v) {
+        Log.d(getClass().getName(), "KJH : " + Thread.currentThread().getStackTrace()[2].getMethodName());
+        MapPoint.GeoCoordinate mapPointGeo = mapPoint.getMapPointGeoCoord();
+        Log.d(getClass().getName(), String.format("(%f, %f) accuracy (%f)", mapPointGeo.latitude, mapPointGeo.longitude, v));
+    }
+
+    @Override
+    public void onCurrentLocationDeviceHeadingUpdate(MapView mapView, float v) {
+
+    }
+
+    @Override
+    public void onCurrentLocationUpdateFailed(MapView mapView) {
+
+    }
+
+    @Override
+    public void onCurrentLocationUpdateCancelled(MapView mapView) {
+
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.setCurrentLocationTrackingMode(MapView.CurrentLocationTrackingMode.TrackingModeOff);
+    }
+
+    private void showDialogForLocationServiceSetting() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+        builder.setTitle(R.string.title_location_service);
+        builder.setMessage(R.string.message_location_service);
+        builder.setCancelable(true);
+        builder.setPositiveButton("설정", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivityForResult(intent, GPS_ENABLE_REQUEST_CODE);
+            }
+        });
+        builder.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        builder.create().show();
+    }
+
+    private boolean checkLocationServiceStatus() {
+        LocationManager locationManager = (LocationManager) activity.getSystemService(context.LOCATION_SERVICE);
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+                || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+    }
+
+
+
+
 }
